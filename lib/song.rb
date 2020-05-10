56ab867acde8adc6dc1aa56920b723b675c953fb
Kyle McLean <kyledmclean@gmail.com>
2020-05-10 00:02:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,7 @@ class Song
     song
   end
   
-  def self.find_by_name(search)
+  def self.find_by_name(name)
     @@all.find{|search| search.name == name}
 #    @@all.find{|song| song.name == song}
   end
